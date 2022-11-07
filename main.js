92b0e264aebdda4fdb6f20bc15659beb11862976
Aleksandr Khomutov <xkhomut@fi.muni.cz>
2022-11-07 12:56:04 +0100

    Fifth stage done

--- main.js
+++ main.js
@@ -10,7 +10,6 @@ let machine = {
 let espresso = {
     tag: 'espresso',
     water: 250,
-    milk: 0,
     beans: 16,
     price: 4
 }
@@ -28,18 +27,13 @@ let cappuccino = {
     beans: 12,
     price: 6
 }
-const actions = ['buy', 'fill', 'take'];
+const actions = ['buy', 'fill', 'take', 'remaining', 'exit'];
 const options = [espresso, latte, cappuccino];
 function optionsString(options){
     let data = '';
     let num = 1;
     for (let i in options){
-        data +=`${num++} - ${options[i].tag}`;
-        //separator
-        if (i !== options.length - 1)
-            data += ', ';
-        else
-            data += ':\n';
+        data +=`${num++} - ${options[i].tag}, `;
     }
     return data;
 }
@@ -49,35 +43,63 @@ ${machine.water} ml of water
 ${machine.milk} ml of milk
 ${machine.beans} g of coffee beans
 ${machine.cups} disposable cups
-$${machine.money} of money
-`);
+$${machine.money} of money`);
 }
 
-display(machine);
-let action = input(`Write action (${actions.toString()}):`);
-switch (action){
-    case actions[0]: //buy
-        let opt = input(`What do you want to buy? ${optionsString(options)}`)
-        opt = (--opt) % options.length;
-        machine.water -= options[opt].water;
-        machine.milk -= options[opt].milk;
-        machine.beans -= options[opt].beans;
-        machine.cups -= 1;
-        machine.money += options[opt].price;
-        break;
-    case actions[1]: //fill
-        machine.water += Number(input('Write how many ml of water the coffee machine you want to add:'));
-        machine.milk += Number(input('Write how many ml of milk the coffee machine you want to add:'));
-        machine.beans += Number(input('Write how many grams of coffee beans machine you want to add:'));
-        machine.cups += Number(input('Write how many disposable cups you want to add:'));
-        break;
-    case actions[2]: //take
-        console.log(`I gave you $${machine.money}`);
-        machine.money = 0;
-        break;
-    default:
-        console.log(`No such action`);
-        break;
+let status = true;
+while (status){
+    let action = input(`Write action (${actions.toString()}):\n`);
+    console.log();
+    switch (action){
+        case actions[0]: //buy
+            let opt = input(`What do you want to buy? ${optionsString(options)}back - to main menu:\n`)
+            if (opt === 'back')
+                break;
+            opt = (--opt) % options.length;
+            if (machine.cups <= 0){
+                console.log('Sorry, not enough disposable cups!');
+                break;
+            }
+            //let's cook
+            let enough = true;
+            for (const key in machine){
+                if (key === 'money'){
+                    machine.money += options[opt].price;
+                    continue;
+                }
+                if (key !== 'cups' && options[opt].hasOwnProperty(key)) {
+                        if (machine[key] < options[opt][key]){
+                            console.log(`Sorry, not enough ${key}!`);
+                            enough = false;
+                            break;
+                        }
+                        machine[key] -= options[opt][key];
+                }
+            }
+            if (enough) {
+                machine.cups--;
+                console.log('I have enough resources, making you a coffee!');
+            }
+            break;
+        case actions[1]: //fill
+            machine.water += +input('Write how many ml of water the coffee machine you want to add:\n');
+            machine.milk += +input('Write how many ml of milk the coffee machine you want to add:\n');
+            machine.beans += +input('Write how many grams of coffee beans machine you want to add:\n');
+            machine.cups += +input('Write how many disposable cups you want to add:\n');
+            break;
+        case actions[2]: //take
+            console.log(`I gave you $${machine.money}`);
+            machine.money = 0;
+            break;
+        case actions[3]: //remaining
+            display(machine);
+            break;
+        case actions[4]: //exit
+            status = false;
+            break;
+        default:
+            console.log(`No such action`);
+            break;
+    }
+    console.log();
 }
-console.log();
-display(machine);
