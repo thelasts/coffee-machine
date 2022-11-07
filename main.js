665456a3b30453bbf0b5472be2f46179d33c0f37
Aleksandr Khomutov <xkhomut@fi.muni.cz>
2022-11-07 09:51:16 +0100

    Third stage done

--- main.js
+++ main.js
@@ -7,9 +7,23 @@ const input = require('sync-input')
 // Pouring coffee into the cup
 // Pouring some milk into the cup
 // Coffee is ready!`)
-
+let water = input('Write how many ml of water the coffee machine has:');
+let milk = input('Write how many ml of milk the coffee machine has:');
+let beans = input('Write how many grams of coffee beans machine has:');
 let userCups = input('Write how many cups of coffee you will need:');
-console.log(`For ${userCups == 1 ? `${userCups} cup` : `${userCups} cups`} of coffee you will need:
-${userCups*200} ml of water
-${userCups*50} ml of milk
-${userCups*15} g of coffee beans`);
+// console.log(`For ${userCups == 1 ? `${userCups} cup` : `${userCups} cups`} of coffee you will need:
+// ${userCups*200} ml of water
+// ${userCups*50} ml of milk
+// ${userCups*15} g of coffee beans`);
+let cups = Math.min(Math.floor(water/200), Math.floor(milk/50), Math.floor(beans/15));
+switch (Math.sign(cups - userCups)){
+    case (-1):
+        console.log(`No, I can make only ${cups} cups of coffee.`);
+        break;
+    case (0):
+        console.log('Yes, I can make that amount of coffee');
+        break;
+    default:
+        console.log(`Yes, I can make that amount of coffee (and even ${cups - userCups} more than that)`);
+        break;
+}
